a6c1b6b8de68654a5eb411fc472de118f0ea6db2
damdrade <damdrade.morais@gmail.com>
2021-03-04 06:35:47 -0300

    First commit

---
+++ Controllers/UploadFileController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using static Subrip.SubtitlesTime;
+
+namespace Subrip.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        public ActionResult Index()
+        {
+            var items = GetFiles();
+            return View(items);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string time, string options)
+        {
+            Regex pattern = new Regex(@"^\d{1,2}:\d{1,2}:\d{1,2},\d{1,3}$");
+
+            if (file != null && file.ContentLength > 0 && Path.GetExtension(file.FileName).Equals(".srt"))
+                if (pattern.Match(time).Success)
+                {
+                    try
+                    {
+                        string path = Path.Combine(Server.MapPath("~/SubRips"), Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        SubtitlesTime.start(path, time, options);
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = "Please enter a valid offset";
+                }
+            else
+            {
+                ViewBag.Message = "Please select a SubRip file.";
+            }
+
+            var items = GetFiles();
+            return View(items);
+        }
+
+        public FileResult Download(string FileName)
+        {
+            var FileVirtualPath = "~/SubRips/" + FileName;
+            return File(FileVirtualPath, "application/force- download", Path.GetFileName(FileVirtualPath));
+        }
+
+        private List<string> GetFiles()
+        {
+            var dir = new System.IO.DirectoryInfo(Server.MapPath("~/SubRips"));
+            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*");
+
+            List<string> items = new List<string>();
+            foreach (var file in fileNames)
+            {
+                items.Add(file.Name);
+            }
+
+            return items;
+        }
+
+    }
+}
---
+++ SubtitlesTime.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Subrip
+{
+    public class SubtitlesTime
+    {
+        private static Match matchParts;
+        private static Regex timeParts = new Regex(@"(\d+):(\d+):(\d+),(\d+)");
+        private static int t1, t2, t3, t4;
+        private static TimeSpan ts;
+
+        public static void start(string filePath, string time, string options)
+        {
+            Regex timeGroup = new Regex(@"^([\d:,]+) --> ([\d:,]+)$");
+            Match matchGroup;
+            string ts1, ts2;
+            TimeSpan offset = convertToTimeSpan(time);
+
+            List<string> lines = new List<string>();
+            List<string> lines2 = new List<string>();
+            lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (String line in lines)
+            {
+                matchGroup = timeGroup.Match(line);
+
+                if (matchGroup.Success)
+                {
+                    ts1 = includeOffset(matchGroup.Groups[1], offset, options);
+                    ts2 = includeOffset(matchGroup.Groups[2], offset, options);
+
+                    lines2.Add($"{ts1} --> {ts2}");
+                }
+                else
+                {
+                    lines2.Add(line);
+                }
+            }
+
+            File.WriteAllLines(filePath, lines2);
+        }
+
+        private static string includeOffset(Group group, TimeSpan offset, string options)
+        {
+            TimeSpan ts = convertToTimeSpan(group.ToString());
+
+            if(options == "Add")
+            {
+                ts = ts.Add(offset);
+            }
+            else
+            {
+                ts = ts.Subtract(offset);
+            }
+
+            return ts.ToString("hh\\:mm\\:ss\\,fff");
+        }
+
+        private static TimeSpan convertToTimeSpan(string offset)
+        {
+            matchParts = timeParts.Match(offset);
+
+            t1 = int.Parse(matchParts.Groups[1].ToString());
+            t2 = int.Parse(matchParts.Groups[2].ToString());
+            t3 = int.Parse(matchParts.Groups[3].ToString());
+            t4 = int.Parse(matchParts.Groups[4].ToString());
+            ts = new TimeSpan(0, t1, t2, t3, t4);
+
+            return ts;
+        }
+    }
+}
